7a6fa453324ccfe0206ccdb653b7882f4d84305d
janak-shrestha <webjanak@gmail.com>
2022-06-11 18:36:04 +0200

    add all files

---
+++ script.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Blogger", "Designer", "Freelancer", "Learner"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Blogger", "Designer", "Freelancer", "Learner"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "images/sun.jpg";
+    }
+    else {
+        icon.src = "images/moon.jpg";
+    }
+}
+
+const skillsSection = document.getElementById('all-skills');
+
+const progressBars = document.querySelectorAll('.progress-bar');
+
+function showProgress() {
+    progressBars.forEach(progressBar => {
+        const value = progressBar.dataset.progress;
+
+        progressBar.style.opacity = 1;
+        progressBar.style.width = `${value}%`;
+    });
+}
+
+function hideProgress() {
+    progressBars.forEach(p => {
+        p.style.opacity = 0;
+        p.style.width = 0;
+    });
+}
+
+window.addEventListener('scroll',() => {
+    const sectionPos = skillsSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight / 2;
+
+    if(sectionPos < screenPos) {
+        showProgress();
+    } else {
+        hideProgress();
+    }
+});
